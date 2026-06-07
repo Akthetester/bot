aaee827291e3318eb5ec5cbc07da3a75815063c1
Akthetester <earnwithak512@gmail.com>
2026-06-07 16:51:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
-from telegram.ext import Application
+from telegram.ext import Application, CommandHandler
+import os
 
-TOKEN = "8699399335:AAHYf8Z60cpzJgVfkQ3ex4QzcaQIOzZnLHY"
+TOKEN = os.getenv("8699399335:AAHYf8Z60cpzJgVfkQ3ex4QzcaQIOzZnLHY")
+
+async def start(update, context):
+    await update.message.reply_text("Bot is running!")
 
 app = Application.builder().token(TOKEN).build()
 
+app.add_handler(CommandHandler("start", start))
+
+print("Bot Started...")
 app.run_polling()
